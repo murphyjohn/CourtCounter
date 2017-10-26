3bc38edb433d02f0bc037b29bb5307ef29339404
John Murphy <murphy.john1991@gmail.com>
2017-10-26 12:11:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given scoreTeamA for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    //Add 3 points to the scoreTeamA and display the new scoreTeamA
+    public void addThreeForTeamA(View view){
+        scoreTeamA +=3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //Add 2 points to the scoreTeamA and display the new scoreTeamA
+    public void addTwoForTeamA(View view){
+        scoreTeamA +=2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //Add 1 point1 to the scoreTeamA and display the new scoreTeamA
+    public void addOneForTeamA(View view){
+        scoreTeamA +=1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    //Add 3 points to the scoreTeamB and display the new scoreTeamB
+    public void addThreeForTeamB(View view){
+        scoreTeamB +=3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //Add 2 points to the scoreTeamB and display the new scoreTeamB
+    public void addTwoForTeamB(View view){
+        scoreTeamB +=2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //Add 1 point1 to the scoreTeamB and display the new scoreTeamB
+    public void addOneForTeamB(View view){
+        scoreTeamB +=1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //Reset button to reset the scores to 0
+    public void resetScores(View view){
+        scoreTeamA=0;
+        scoreTeamB=0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+}
